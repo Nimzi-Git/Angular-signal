bc4e26fdd1c4b99943b551b73c020d0d9627d61b
Nimzi-Git <itz.nimz@gmail.com>
2025-02-27 14:07:36 +0000

    Added basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/dashboard'
+    }, 
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/dashboard'
+    }
+];
